68d0bb8b38e1eaf8ba8f3a7257c3d5390888ae2b
BJRambo <qw5414@naver.com>
2017-11-09 15:54:49 +0900

    Fix to user_id is null

--- something.controller.php
+++ something.controller.php
@@ -1,24 +1,38 @@
 <?php
 class somethingController extends something
 {
+
 	function triggerBeforeModuleHandlerInit(&$oModule)
 	{
+		$memberStrMatch = preg_match('/^(?:disp|proc)Member/u', $oModule->act);
+		if($memberStrMatch)
+		{
+			return;
+		}
 		/** @var $oModuleModel moduleModel */
 		$oModuleModel = getModel('module');
 		$vid = Context::get('vid');
-		// HACK(BJRambo): 강제적으로 mid를이용해서 유저 아이디를 선언
-		$userId = $oModule->mid;
-		Context::set('user_id', $userId);
+
 
 		// Vid is not mid. so change vid to mid.
 		if($vid)
 		{
+			// HACK(BJRambo): 강제적으로 mid를이용해서 유저 아이디를 선언
+			$userId = $oModule->mid;
+
 			$moduleInfo = $oModuleModel->getModuleInfoByMid($vid);
+			debugPRint($moduleInfo);
+			if($moduleInfo->module !== 'something')
+			{
+				return;
+			}
+
 
 			$this->module = $moduleInfo->module;
 			$this->mid = $moduleInfo->mid;
 			$oModule->module = $moduleInfo->module;
 			$oModule->mid = $moduleInfo->mid;
 		}
+		Context::set('user_id', $userId);
 	}
 }
