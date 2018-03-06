5cf4f058918cb2958073d3090d07803e0368426c
BJRambo <sosifam@070805.co.kr>
2018-03-07 07:36:03 +0900

    스킨 설정이 정상적으로 선택이 안되어잇는 문제 고침.

--- something.admin.view.php
+++ something.admin.view.php
@@ -15,11 +15,12 @@ class somethingAdminView extends something
 
 		Context::set('config', $config);
 
-		$oModuleModel = &getModel('module');
+		$oModuleModel = getModel('module');
 
-		if ($config->mid_name != "")
+		if ($config->mid_name)
 		{
-			Context::set('module_info', $oModuleModel->getModuleInfoByMid($config->mid_name));
+			$module_info = $oModuleModel->getModuleInfoByMid($config->mid_name);
+			Context::set('module_info', $module_info);
 		}
 
 		$skin_list = $oModuleModel->getSkins($this->module_path);
@@ -29,7 +30,7 @@ class somethingAdminView extends something
 		Context::set('mskin_list', $mskin_list);
 
 
-		$oLayoutMode = &getModel('layout');
+		$oLayoutMode = getModel('layout');
 		$layout_list = $oLayoutMode->getLayoutList();
 		Context::set('layout_list', $layout_list);
 
